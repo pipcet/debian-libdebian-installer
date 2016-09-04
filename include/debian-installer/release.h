d49f550889797dd5a4461c99c1a4db90bb2c8949
Bastian Blank <waldi@debian.org>
2016-09-04 19:35:20 +0200

    Drop old Release file implementation
    Signed-off-by: Bastian Blank <waldi@debian.org>

--- include/debian-installer/release.h
+++ include/debian-installer/release.h
@@ -20,66 +20,10 @@
 #ifndef DEBIAN_INSTALLER__RELEASE_H
 #define DEBIAN_INSTALLER__RELEASE_H
 
-#include <debian-installer/hash.h>
-#include <debian-installer/parser.h>
-#include <debian-installer/slist.h>
-
-typedef struct di_release di_release;
-typedef struct di_release_file di_release_file;
-
 /**
  * @addtogroup di_release
  * @{
  */
-
-/**
- * @brief Release file
- */
-struct di_release
-{
-  char *origin;                                 /**< Origin field */
-  char *suite;                                  /**< Suite field */
-  char *codename;                               /**< Codename field */
-  di_hash_table *md5sum;                        /**< checksum fields, includes di_release_file */
-  di_mem_chunk *release_file_mem_chunk;         /**< @internal */
-};
-
-/**
- * @brief Release file - file entry
- */
-struct di_release_file
-{
-  union
-  {
-    char *filename;                             /**< filename */
-    di_rstring key;                             /**< @internal */
-  };
-  unsigned int size;                            /**< size */
-  char *sum[2];                                 /**< checksums, currently md5 and sha1 */
-};
-
-di_release *di_release_alloc (void);
-void di_release_free (di_release *packages);
-
-/**
- * @}
- * @addtogroup di_release_parser
- * @{
- */
-
-di_release *di_release_read_file (const char *file);
-
-/** @} */
-
-di_parser_fields_function_read
-  di_release_parser_read_file;
-
-/**
- * @addtogroup di_release_parser
- * @{
- */
-
-extern const di_parser_fieldinfo *di_release_parser_fieldinfo[];
 
 /** @} */
 #endif
