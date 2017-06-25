cd84db7b95f50f80ef13b77e7be9fb1ff1d5dbfe
Aurelien Jarno <aurelien@aurel32.net>
2017-06-25 18:05:11 +0200

    subarch-mips-linux.c: Add support for checking the "isa" field.

--- src/system/subarch-mips-linux.c
+++ src/system/subarch-mips-linux.c
@@ -9,31 +9,32 @@
 struct hwmap_type {
 	const char *sys_regex;
 	const char *cpu_regex;
+	const char *isa_regex;
 	const char *ret;
 };
 
 static struct hwmap_type hwmap[] = {
-	{"MIPS Malta",		"MIPS (4K|24K|34K|74K)",	"4kc-malta" },
-	{"MIPS Malta",		"MIPS (5K|20K)",		"5kc-malta" },
-	{".*",			"ICT Loongson-2 V0.2",		"loongson-2e" },
-	{".*",			"ICT Loongson-2 V0.3",		"loongson-2f" },
-	{".*",			"ICT Loongson-3",		"loongson-3" },
-	{".*",			"Cavium Octeon",		"octeon" },
-	{"SGI (Indy|Indigo2)",	"R4",				"r4k-ip22" },
-	{"SGI (Indy|Indigo2)",	"R5000",			"r5k-ip22" },
-	{"SGI (Indy|Indigo2)",	"R8000",			"r8k-ip26" },
-	{"SGI (Indy|Indigo2)",	"R10000",			"r10k-ip28" },
-	{"SGI Origin",		"R10000",			"r10k-ip27" },
-	{"SGI Origin",		"R12000",			"r12k-ip27" },
-	{"SGI (O2|IP32)",	"R5|Nevada|RM7000",		"r5k-ip32" },
-	{"SGI (O2|IP32)",	"RM10000",			"r10k-ip32" },
-	{"SGI (O2|IP32)",	"RM12000",			"r12k-ip32" },
-	{"SiByte BCM91250",	"SiByte SB1",			"sb1-bcm91250a" },
-	{"SiByte BCM91.80",	"SiByte SB1A",			"sb1a-bcm91480b" },
+	{"MIPS Malta",		"MIPS (4K|24K|34K|74K)",	".*",		"4kc-malta" },
+	{"MIPS Malta",		"MIPS (5K|20K)",		".*",		"5kc-malta" },
+	{".*",			"ICT Loongson-2 V0.2",		".*",		"loongson-2e" },
+	{".*",			"ICT Loongson-2 V0.3",		".*",		"loongson-2f" },
+	{".*",			"ICT Loongson-3",		".*",		"loongson-3" },
+	{".*",			"Cavium Octeon",		".*",		"octeon" },
+	{"SGI (Indy|Indigo2)",	"R4",				".*",		"r4k-ip22" },
+	{"SGI (Indy|Indigo2)",	"R5000",			".*",		"r5k-ip22" },
+	{"SGI (Indy|Indigo2)",	"R8000",			".*",		"r8k-ip26" },
+	{"SGI (Indy|Indigo2)",	"R10000",			".*",		"r10k-ip28" },
+	{"SGI Origin",		"R10000",			".*",		"r10k-ip27" },
+	{"SGI Origin",		"R12000",			".*",		"r12k-ip27" },
+	{"SGI (O2|IP32)",	"R5|Nevada|RM7000",		".*",		"r5k-ip32" },
+	{"SGI (O2|IP32)",	"RM10000",			".*",		"r10k-ip32" },
+	{"SGI (O2|IP32)",	"RM12000",			".*",		"r12k-ip32" },
+	{"SiByte BCM91250",	"SiByte SB1",			".*",		"sb1-bcm91250a" },
+	{"SiByte BCM91.80",	"SiByte SB1A",			".*",		"sb1a-bcm91480b" },
 
 	/* add new hwmap here */
 
-	{ ".*",			".*",				"unknown" }
+	{ ".*",			".*",				".*", 	"unknown" }
 };
 
 #define BUFFER_LENGTH (1024)
@@ -43,7 +44,7 @@ const char *di_system_subarch_analyze(void)
 	FILE *file;
 	char buf[BUFFER_LENGTH];
 	const char *pos;
-	char *system = NULL, *cpu = NULL;
+	char *system = NULL, *cpu = NULL, *isa = NULL;
 	size_t len, i;
 
 	file = fopen("/proc/cpuinfo", "r");
@@ -58,6 +59,8 @@ const char *di_system_subarch_analyze(void)
 				system = strdup(pos + len);
 			if (!strncmp(buf, "cpu model", strlen("cpu model")))
 				cpu = strdup(pos + len);
+			if (!strncmp(buf, "isa", strlen("isa")))
+				isa = strdup(pos + len);
 		}
 
 		fclose(file);
@@ -66,6 +69,8 @@ const char *di_system_subarch_analyze(void)
 	if (!system)
 		system = strdup("");
 	if (!cpu)
+		cpu = strdup("");
+	if (!isa)
 		cpu = strdup("");
 
 	for (i = 0; i < sizeof(hwmap) / sizeof(struct hwmap_type) ; i++) {
@@ -88,7 +93,15 @@ const char *di_system_subarch_analyze(void)
 		if (ret == REG_NOMATCH)
 			continue;
 
-		/* Match both system and cpu type. Stop. */
+		/* Check for matching isa type */
+		if (regcomp(&preg, hwmap[i].isa_regex, REG_NOSUB | REG_EXTENDED))
+			continue;
+		ret = regexec(&preg, isa, 0, NULL, 0);
+		regfree(&preg);
+		if (ret == REG_NOMATCH)
+			continue;
+
+		/* Match system, cpu and isa type. Stop. */
 		break;
 	}
 
