44e0345cb691421e8fd7d178f68535f1b1e3dedd
Aurelien Jarno <aurelien@aurel32.net>
2017-11-19 17:29:05 +0100

    src/exec.c: Include poll.h instead of sys/poll.h.  Closes: #861598.

--- src/exec.c
+++ src/exec.c
@@ -25,10 +25,10 @@
 
 #include <errno.h>
 #include <fcntl.h>
+#include <poll.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/poll.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
