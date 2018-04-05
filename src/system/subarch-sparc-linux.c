4c1adc34bcc1511234a8567b962c83197004930a
Michael Karcher <debian@mkarcher.dialup.fu-berlin.de>
2018-04-05 14:00:33 +0200

    subarch-sparc-linux.c: Fix return value of di_system_subarch_analyze

--- src/system/subarch-sparc-linux.c
+++ src/system/subarch-sparc-linux.c
@@ -6,14 +6,43 @@
 
 #define BUFFER_LENGTH (1024)
 
+typedef struct {
+  const char *cpuinfo_type;
+  const char *sun_subarch;
+  const char *gpt_subarch;
+} subarch_map_t;
+
+static const char *subarch_string(const char *cpuinfo_type, int is_gpt)
+{
+  static const subarch_map_t subarch_map[] = {
+    { "sun4m", "sun4m_sun", "sun4m_gpt" },
+    { "sun4u", "sun4u_sun", "sun4u_gpt" },
+    { "sun4v", "sun4v_sun", "sun4v_gpt" },
+  };
+  size_t i;
+  for (i = 0; i < sizeof(subarch_map)/sizeof(subarch_map[0]); i++)
+  {
+    if (!strncasecmp(subarch_map[i].cpuinfo_type, cpuinfo_type, strlen(subarch_map[i].cpuinfo_type)))
+    {
+      if (is_gpt)
+        return subarch_map[i].gpt_subarch;
+      else
+        return subarch_map[i].sun_subarch;
+    }
+  }
+  return NULL;
+}
+
 const char *di_system_subarch_analyze(void) 
 {
   FILE *file;
   char buf[BUFFER_LENGTH];
   const char *pos;
-  char *subarch = NULL;
-  const char *label = NULL;
+  const char *subarch = NULL;
   size_t len;
+  int is_gpt;
+
+  is_gpt = access("/sys/firmware/devicetree/base/packages/disk-label/gpt", F_OK) != -1;
 
   file = fopen("/proc/cpuinfo", "r");
 
@@ -24,23 +53,11 @@ const char *di_system_subarch_analyze(void)
       if (!(len = strspn(pos, ": \t")))
 	continue;
       if (!strncmp(buf, "type", strlen("type")))
-	subarch = strndup(pos + len, strlen(pos + len) - 1);
+	subarch = subarch_string(pos + len, is_gpt);
     }
 
     fclose(file);
   }
 
-  if (!subarch)
-    subarch = strdup("unknown");
-
-  if (access("/sys/firmware/devicetree/base/packages/disk-label/gpt", F_OK) != -1)
-    label = "gpt";
-  else
-    label = "sun";
-
-  snprintf(buf, BUFFER_LENGTH, "%s_%s", subarch, label);
-
-  free(subarch);
-
-  return buf;
+  return subarch;
 }
